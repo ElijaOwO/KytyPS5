1ea57b1e4b851aaf15a187fe206e89de8e9da0a0
Elija <143048359+ElijaOwO@users.noreply.github.com>
2026-09-18 20:59:54 +0200

    BUG-0006: split indirect image source kinds

--- src/graphics/shader/recompiler/ir/ShaderIR.h
+++ src/graphics/shader/recompiler/ir/ShaderIR.h
@@ -456,12 +456,18 @@ struct BlockInfo {
 };
 
 struct DescriptorSource {
+	enum class IndirectImageKind { MaterialTable, AddressArray };
+
 	struct IndirectImage {
-		uint32_t material_source = 0;
-		uint32_t heap_source     = 0;
-		uint32_t selector_stride = 0;
-		uint32_t selector_offset = 0;
-		uint32_t key_arg         = 0;
+		uint32_t          material_source  = 0;
+		uint32_t          heap_source      = 0;
+		uint32_t          selector_stride  = 0;
+		uint32_t          selector_offset  = 0;
+		uint32_t          key_arg          = 0;
+		IndirectImageKind kind             = IndirectImageKind::MaterialTable;
+		uint32_t          address_source   = 0;
+		uint32_t          descriptor_stride = 0;
+		uint32_t          candidate_count  = 0;
 
 		bool operator==(const IndirectImage& other) const = default;
 	};
